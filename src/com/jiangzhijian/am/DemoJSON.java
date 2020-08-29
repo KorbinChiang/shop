f058123042b4c99e04ff529b9efdf36f4865c33f
蒋志坚 <KorbinChiang@163.com>
2020-08-29 19:05:17 +0800

    java

---
+++ src/com/jiangzhijian/am/DemoJSON.java
@@ -0,0 +1,12 @@
+package com.jiangzhijian.am;
+
+import java.util.Scanner;
+
+public class DemoJSON {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+//        sc.close();
+        System.out.println("a = " + a);
+    }
+}
