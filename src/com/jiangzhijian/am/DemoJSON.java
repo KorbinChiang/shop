c46564c285af2af512b008682b31ecbff62ba65a
蒋志坚 <KorbinChiang@163.com>
2022-04-12 10:32:27 +0800

    delete useless

--- src/com/jiangzhijian/am/DemoJSON.java
+++ src/com/jiangzhijian/am/DemoJSON.java
@@ -6,7 +6,6 @@ public class DemoJSON {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-//        sc.close();
         System.out.println("a = " + a);
     }
 }
