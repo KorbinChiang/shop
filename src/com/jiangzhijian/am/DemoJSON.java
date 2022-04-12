551b5df2eeca205743d47d466b656229d2ab59d8
蒋志坚 <KorbinChiang@163.com>
2022-04-12 11:42:53 +0800

    添加忽略文件

--- src/com/jiangzhijian/am/DemoJSON.java
+++ src/com/jiangzhijian/am/DemoJSON.java
@@ -3,9 +3,11 @@ package com.jiangzhijian.am;
 import java.util.Scanner;
 
 public class DemoJSON {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println("a = " + a);
     }
+
 }
